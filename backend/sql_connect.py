bbb5de7c983a7dc93b05c9082862f2240527de60
Ankit Rajput <113281225+ankitrajput77@users.noreply.github.com>
2025-02-06 23:02:43 +0530

    Update sql_connect.py

--- backend/sql_connect.py
+++ backend/sql_connect.py
@@ -7,7 +7,7 @@ __cnx = None
 def connect_sql():
     global __cnx 
     if __cnx == None :
-        __cnx = pymysql.connect(user = 'root', password = '8287262684@#$aR', database = 'grocery_store')
+        __cnx = pymysql.connect(user = 'root', password = '', database = 'grocery_store')
     print('you got connected')
     return __cnx
 
